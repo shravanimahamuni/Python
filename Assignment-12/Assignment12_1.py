e19608d70c6cc9e1f77e5ba693bc38f747f43aa6
shravanimahamuni <shravanimahamuni9811@gmail.com>
2026-07-03 23:29:30 +0530

    Add files via upload

---
+++ Assignment-12/Assignment12_1.py
@@ -0,0 +1,9 @@
+# Write a programs are written using only the concepts covered in your notes (def, if-else, for, while, range(), return, print()).
+
+def ChkVowel(ch):
+    if ch =='a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+        print("Vowels")
+    else:
+        print("Consonant")
+
+ChkVowel('a')
